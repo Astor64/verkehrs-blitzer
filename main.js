762099927183198784cc365be8408f2946bdcb43
Wolfgang Huesgen <wolfgang.huesgen@web.de>
2025-04-26 14:57:49 +0200

    index.html main.js revsed speedCamera

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function main() {
+  let inputSpeed = document.getElementById("speed");
+  let inputDirection = document.getElementById("direction");
+  let inputSpeedCamera = document.getElementById("speedCamera");
+  let speed = inputSpeed.value;
+  let isFacing = inputDirection.checked;
+  let speedCamera = inputSpeedCamera.value;
+
+  if (speed > (speedCamera / 100) * 10 + speedCamera && isFacing == true) {
+    alert("Du wurdest geblitzt");
+  }
+
+  // ein inputfeld zur Geschwindigkeitseingabe plus 10% Toleranz
+  // console.log(speed);
+
+  // console.log(isFacing);
+}
